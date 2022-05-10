689f6aea51a3fa2ad62e06df871c35e1e4428de0
baiorett <70862504+baiorett@users.noreply.github.com>
2022-05-10 19:30:41 +0300

    cards

--- app/src/main/java/app/revanced/manager/ui/screens/mainsubscreens/FeedSubscreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screens/mainsubscreens/FeedSubscreen.kt
@@ -1,6 +1,7 @@
 package app.revanced.manager.ui.screens.mainsubscreens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
@@ -18,27 +19,67 @@ import androidx.compose.ui.unit.sp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FeedSubscreen() {
-    LazyVerticalGrid(modifier = Modifier.padding(8.dp), columns = GridCells.Adaptive(100.dp)) {
-        item() {
-            Card(modifier = Modifier
+    Column(modifier = Modifier.padding(16.dp)) {
+        Card(
+            modifier = Modifier
                 .padding(4.dp)
-                .sizeIn(minWidth = 250.dp, minHeight = 150.dp)) {
+                .fillMaxWidth()
+                .height(150.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "COPE",
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.Medium,
+                    lineHeight = 24.sp
+                )
+            }
+        }
+        Row() {
+            Card(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .sizeIn(minWidth = 200.dp, minHeight = 150.dp, maxWidth = 200.dp)
+            ) {
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "test", fontSize = 16.sp, fontFamily = FontFamily.Default, fontWeight = FontWeight.Medium, lineHeight = 24.sp)
+                    Text(
+                        text = "I don't follow the Material You guidelines",
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily.Default,
+                        fontWeight = FontWeight.Medium,
+                        lineHeight = 24.sp
+                    )
+                }
+            }
+            Card(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .sizeIn(minWidth = 250.dp, minHeight = 150.dp)
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "So what?",
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily.Default,
+                        fontWeight = FontWeight.Medium,
+                        lineHeight = 24.sp
+                    )
+                    Text(
+                        text = "desc",
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily.Default,
+                        fontWeight = FontWeight.Medium,
+                        lineHeight = 24.sp
+                    )
                 }
             }
         }
-        item {
-            Card(modifier = Modifier.padding(4.dp)) {
-                Text(text = "eeeeeeeee")
-            }
-        }
-        item {
-            Card(modifier = Modifier.padding(4.dp)) {
-                Text(text = "eaaaaaaaaaaarewerwaa")
-            }
-        }
+
+
+
     }
+
 }
 
 @Preview
