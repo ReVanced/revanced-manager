108e727cbcdd9ffeee4c85a5b7b35bd9a988ed5c
Lucaskyy <contact@sculas.xyz>
2022-05-11 02:05:47 +0200

    refactor: cleanup code

--- app/src/main/java/app/revanced/manager/ui/components/AppList.kt
+++ app/src/main/java/app/revanced/manager/ui/components/AppList.kt
@@ -4,10 +4,8 @@ import android.content.pm.ApplicationInfo
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ListItem
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import app.revanced.manager.ui.components.placeholders.applist.AppIcon
 
--- app/src/main/java/app/revanced/manager/ui/screens/AppSelectorScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screens/AppSelectorScreen.kt
@@ -1,11 +1,7 @@
 package app.revanced.manager.ui.screens
 
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,8 +14,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import app.revanced.manager.R
 import app.revanced.manager.ui.components.AppList
 import app.revanced.manager.ui.components.DialogAppBar
-
-const val TAG = "ReVanced"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
