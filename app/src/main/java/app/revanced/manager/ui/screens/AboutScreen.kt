1cc7da7d30e3d9e5b2750bf2e85e33de29cb6f70
Aunali321 <aunvakil.aa@gmail.com>
2022-06-29 20:29:25 +0530

    Added temp revanced logo.

--- app/src/main/java/app/revanced/manager/ui/screens/AboutScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screens/AboutScreen.kt
@@ -7,11 +7,14 @@ import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ListItem
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +22,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +39,7 @@ import app.revanced.manager.ui.components.placeholders.applist.AppIcon
 import app.revanced.manager.ui.screens.mainsubscreens.PatcherViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import app.revanced.manager.R
 
 private const val tag = "AboutScreen"
 
@@ -41,13 +50,27 @@ private const val tag = "AboutScreen"
 fun AboutScreen(
     //navigator: NavController,
 ) {
-    Column() {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = "ReVanced Manager",
-                style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.align(Alignment.TopCenter).paddingFromBaseline(12.dp,0.dp)
-            )
-        }
+    Column(Modifier.padding(8.dp)) {
+//        Box() {
+//            Text(
+//                text = "ReVanced Manager",
+//                style = MaterialTheme.typography.headlineMedium,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .align(Alignment.TopCenter)
+//                    .padding(20.dp,0.dp,0.dp,12.dp),
+//                textAlign = TextAlign.Center
+//            )
+//        }
+        Image(
+            painterResource(R.drawable.revancedtext),
+            contentDescription = "ReVanced Logo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+        )
+        Divider(Modifier.alpha(.5f))
+
     }
+
 }
