aa91139f63314e198b436d54075254a1f00921aa
Ushie <ushie@users.noreply.github.com>
2022-06-29 20:50:13 +0300

    Rough text buttons

--- app/src/main/java/app/revanced/manager/ui/screens/AboutScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screens/AboutScreen.kt
@@ -1,14 +1,28 @@
 package app.revanced.manager.ui.screens
 
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.IconButton
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
@@ -16,6 +30,7 @@ import com.ramcosta.composedestinations.annotation.RootNavGraph
 import app.revanced.manager.R
 import app.revanced.manager.Global.Companion.socialLinks
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.stringResource
 
 private const val tag = "AboutScreen"
 
@@ -47,8 +62,23 @@ fun AboutScreen(
                 .height(150.dp)
         )
         Divider(Modifier.alpha(.5f))
-
+        
         var currentUriHandler = LocalUriHandler.current
+
+        PreferenceRow(
+            title = stringResource(R.string.whats_new),
+            onClick = { currentUriHandler.openUri("https://revanced.app") },
+        )
+
+        PreferenceRow(
+            title = stringResource(R.string.help_translate),
+            onClick = { currentUriHandler.openUri("https://revanced.app") },
+        )
+        
+        PreferenceRow(
+            title = stringResource(R.string.help_translate),
+            onClick = { currentUriHandler.openUri("https://revanced.app") },
+        )
 
         Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             for ((social_ic, uri) in socialLinks.entries) {
@@ -58,7 +88,71 @@ fun AboutScreen(
             }
         }
     }
-    
+}
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PreferenceRow(
+    modifier: Modifier = Modifier,
+    title: String,
+    painter: Painter? = null,
+    onClick: () -> Unit = {},
+    onLongClick: () -> Unit = {},
+    subtitle: String? = null,
+    action: @Composable (() -> Unit)? = null,
+) {
+    val height = if (subtitle != null) 72.dp else 56.dp
 
+    val titleTextStyle = MaterialTheme.typography.bodyLarge
+    val subtitleTextStyle = MaterialTheme.typography.bodyMedium.copy(
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.75f),
+    )
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = height)
+            .combinedClickable(
+                onLongClick = onLongClick,
+                onClick = onClick,
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        if (painter != null) {
+            Icon(
+                painter = painter,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp)
+                    .size(24.dp),
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null,
+            )
+        }
+        Column(
+            Modifier
+                .padding(horizontal = 16.dp)
+                .weight(1f),
+        ) {
+            Text(
+                text = title,
+                style = titleTextStyle,
+            )
+            if (subtitle != null) {
+                Text(
+                    modifier = Modifier.padding(top = 4.dp),
+                    text = subtitle,
+                    style = subtitleTextStyle,
+                )
+            }
+        }
+        if (action != null) {
+            Box(
+                Modifier
+                    .widthIn(min = 56.dp)
+                    .padding(end = 16.dp),
+            ) {
+                action()
+            }
+        }
+    }
 }
