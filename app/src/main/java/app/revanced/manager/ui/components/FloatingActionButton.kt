469987be1fa1c5daf4e1d1a9e1be18e55bffde08
Aunali321 <aunvakil.aa@gmail.com>
2022-07-02 22:55:15 +0530

    feat: toast to tell users to select apk before spamming patch button.

--- app/src/main/java/app/revanced/manager/ui/components/FloatingActionButton.kt
+++ app/src/main/java/app/revanced/manager/ui/components/FloatingActionButton.kt
@@ -1,5 +1,6 @@
 package app.revanced.manager.ui.components
 
+import android.widget.Toast
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material.ripple.RippleTheme
@@ -8,6 +9,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
 
 @Composable
 fun FloatingActionButton(
@@ -16,6 +19,8 @@ fun FloatingActionButton(
     onClick: () -> Unit,
     enabled: Boolean
 ) {
+    val context = LocalContext.current
+
     // TODO: set icon color:
     // tint = if (enabled) LocalContentColor.current.copy(alpha = LocalContentAlpha.current) else else DarkGray
     CompositionLocalProvider(
@@ -27,7 +32,12 @@ fun FloatingActionButton(
             text = text,
             icon = icon,
             onClick = {
+                if (!enabled) {
+                    Toast.makeText(context, "Please select an application to patch", Toast.LENGTH_LONG).show()
+                }
                 if (enabled) onClick()
+
+
             },
             containerColor = if (enabled) MaterialTheme.colorScheme.primaryContainer else Color.Gray,
         )
