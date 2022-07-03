265c2c25ec0e704f8d57a50f02775708dd5ddf69
Ushie <ushie@users.noreply.github.com>
2022-07-03 20:49:09 +0300

    feat: added copy to about version

--- app/src/main/java/app/revanced/manager/ui/components/AboutDialog.kt
+++ app/src/main/java/app/revanced/manager/ui/components/AboutDialog.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import app.revanced.manager.BuildConfig
 import app.revanced.manager.R
@@ -24,6 +26,8 @@ fun AboutDialog() {
 
     val onPopupDismissed = { showPopup = false }
 
+    val localClipboardManager = LocalClipboardManager.current
+
     PreferenceRow(
         title = stringResource(R.string.app_version),
         subtitle = "${BuildConfig.VERSION_TYPE} ${BuildConfig.VERSION_NAME}",
@@ -32,29 +36,30 @@ fun AboutDialog() {
     )
 
     if (showPopup) {
-    AlertDialog(
-        backgroundColor = MaterialTheme.colorScheme.background,
-        onDismissRequest = onPopupDismissed,
-        text = {
-            Column(Modifier.padding(8.dp)) {
-                Text(text = "Application Version = " + BuildConfig.VERSION_NAME)
-                Text(text = "Version Type = " + BuildConfig.VERSION_TYPE)
-                Text(text = "Build Type = " + BuildConfig.BUILD_TYPE)
-            }
-        },
-        confirmButton = {
-
-        },
-        dismissButton = {
-            TextButton(onClick = { onPopupDismissed() }) {
-                Text(text = "Close")
-            }
-        },
-        title = {
-            Text(
-                text = stringResource(R.string.app_version)
-            )
-        },
-    )}
+        AlertDialog(
+            backgroundColor = MaterialTheme.colorScheme.background,
+            onDismissRequest = onPopupDismissed,
+            text = {
+                Column(Modifier.padding(8.dp)) {
+                    Text(text = DebugInfo())
+                }
+            },
+            // TODO: MAKE CLIPBOARD REUSABLE, ADD TOAST MESSAGE *CLEANLY*
+            confirmButton = {
+                TextButton(onClick = { localClipboardManager.setText(AnnotatedString(DebugInfo())) }) {
+                    Text(text = "Copy")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { onPopupDismissed() }) {
+                    Text(text = "Close")
+                }
+            },
+            title = {
+                Text(
+                    text = stringResource(R.string.app_version)
+                )
+            },
+        )}
 
 }
