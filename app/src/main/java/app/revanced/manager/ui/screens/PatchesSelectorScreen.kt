a0a4d75b6d13b5096d77f51afb09d1919898ecda
Ushie <ushiekane@gmail.com>
2022-07-11 19:53:38 +0300

    feat: combine version and unsupported into chip

--- app/src/main/java/app/revanced/manager/ui/screens/PatchesSelectorScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screens/PatchesSelectorScreen.kt
@@ -1,15 +1,12 @@
 package app.revanced.manager.ui.screens
 
 import ExpandingText
-import app.revanced.manager.ui.components.PatchCompatibilityDialog
 import android.annotation.SuppressLint
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -17,13 +14,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import app.revanced.manager.R
 import app.revanced.manager.ui.Resource
 import app.revanced.manager.ui.components.LoadingIndicator
+import app.revanced.manager.ui.components.PatchCompatibilityDialog
 import app.revanced.manager.ui.screens.mainsubscreens.PatchClass
 import app.revanced.manager.ui.screens.mainsubscreens.PatcherViewModel
 import app.revanced.manager.ui.theme.Typography
@@ -149,37 +147,29 @@ fun PatchSelectable(patchClass: PatchClass, isSelected: Boolean, onSelected: () 
                     )
                 }
 
-                if (patchClass.unsupported) {
-                    Column(Modifier.align(Alignment.CenterVertically)) {
-                        Text(
-                            text = "⚠️ Unsupported!",
-                            modifier = Modifier.padding(horizontal = 4.dp),
-                            style = Typography.bodySmall,
-                            color = Color.Yellow
-                        )
-                    }
-                }
-
                 Column {
                     Row(
                         horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Column {
-                            Text(
-                                text = patch.version ?: "unknown",
-                                style = Typography.bodySmall,
-                                modifier = Modifier
-                                    .absolutePadding(24.dp, 0.dp, 12.dp, 0.dp)
-                            )
-                        }
 
                         if (showDialog) {
                             PatchCompatibilityDialog(onClose = { showDialog = false }, patchClass = patchClass)
                         }
-                        IconButton(onClick =  {showDialog = true}) {
-                            Icon(painter = painterResource(id = R.drawable.ic_baseline_info_24), contentDescription = null)
-                        }
+                        InputChip(
+                            modifier = Modifier.padding(horizontal = 8.dp),
+                            onClick = { showDialog = true },
+                            leadingIcon = {
+                                if (patchClass.unsupported) Icon(
+                                    Icons.Default.Warning,
+                                    contentDescription = stringResource(id = R.string.unsupported_version)
+                                ) else Icon(
+                                    Icons.Default.Info,
+                                    contentDescription = stringResource(id = R.string.version_info)
+                                )
+                            },
+                            label = { Text(text = patch.version ?: "unknown") }
+                        )
 
                         CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
                             Checkbox(
