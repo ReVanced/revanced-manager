5542e9b9438e1b2251282330f3d209c99a452c58
Aunali321 <aunvakil.aa@gmail.com>
2022-08-02 13:36:35 +0530

    feat: fully complete manager api.

--- lib/backend/api/manager_api.dart
+++ lib/backend/api/manager_api.dart
@@ -5,41 +5,53 @@ import 'package:revanced_manager_flutter/constants.dart';
 import 'github_api.dart';
 
 // use path_provider to get the path of the storage directory
-Dio dio = Dio();
-GithubAPI githubAPI = GithubAPI();
 
-Future<String?> getPath() async {
-  final path = await p.getApplicationSupportDirectory();
-  final workDir = Directory('${path.path}/revanced').createSync();
-  final workDirPath = "${path.path}/revanced";
-  return workDirPath;
-}
+class ManagerAPI {
+  Dio dio = Dio();
+  GithubAPI githubAPI = GithubAPI();
 
-Future<File?> downloadAssets(String repo) async {
-  try {
-    final workDir = await getPath();
-    final dlUrl = await githubAPI.latestRelease(ghOrg, repo);
-    final name =
-        dlUrl?.split('/').lastWhere((element) => element.contains('revanced'));
-    print(name);
-    final assetFile = File('$workDir/$name');
-    final response = await dio.get(
-      dlUrl!,
-      options: Options(
-        responseType: ResponseType.bytes,
-        followRedirects: true,
-        receiveTimeout: 0,
-      ),
-      onReceiveProgress: (count, total) {
-        print('$count/$total');
-      },
-    );
-    final raf = assetFile.openSync(mode: FileMode.write);
-    raf.writeFromSync(response.data);
-    raf.closeSync();
-    return assetFile;
-  } catch (e) {
-    print(e);
-    return null;
+  Future<String?> getPath() async {
+    final path = await p.getApplicationSupportDirectory();
+    final workDir = Directory('${path.path}/revanced').createSync();
+    final workDirPath = "${path.path}/revanced";
+    return workDirPath;
+  }
+
+  Future<File?> downloadAssets(String repo) async {
+    try {
+      final workDir = await getPath();
+      final dlUrl = await githubAPI.latestRelease(ghOrg, repo);
+      final name = dlUrl
+          ?.split('/')
+          .lastWhere((element) => element.contains('revanced'));
+      print(name);
+      final assetFile = File('$workDir/$name');
+      final response = await dio.get(
+        dlUrl!,
+        options: Options(
+          responseType: ResponseType.bytes,
+          followRedirects: true,
+          receiveTimeout: 0,
+        ),
+        onReceiveProgress: (count, total) {
+          print('$count/$total');
+        },
+      );
+      final raf = assetFile.openSync(mode: FileMode.write);
+      raf.writeFromSync(response.data);
+      raf.closeSync();
+      return assetFile;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<void> downloadPatches() async {
+    await downloadAssets(patchesRepo);
+  }
+
+  Future<void> downloadIntegrations() async {
+    await downloadAssets(integrationsRepo);
   }
 }
