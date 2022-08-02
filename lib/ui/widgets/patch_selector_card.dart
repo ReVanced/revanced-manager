fa8fa5c82089e7c31812393362c1a216be85a966
Aunali321 <aunvakil.aa@gmail.com>
2022-08-02 14:03:46 +0530

    feat: primitive Patcher screen.

---
+++ lib/ui/widgets/patch_selector_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:revanced_manager_flutter/constants.dart';
+
+class PatchSelectorCard extends StatelessWidget {
+  final Function()? onPressed;
+  const PatchSelectorCard({
+    Key? key,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: const Color(0xff1B222B),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Select patches",
+              style: GoogleFonts.roboto(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Select an application first.",
+              style: robotoTextStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
