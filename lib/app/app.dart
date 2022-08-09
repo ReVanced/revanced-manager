6338eb5dd195e698789676cb04187c43f14afa5a
Alberto Ponces <ponces26@gmail.com>
2022-08-09 01:33:37 +0100

    feat: register Patcher View Models

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,11 @@
 import 'package:revanced_manager/services/patcher_api.dart';
 import 'package:revanced_manager/ui/views/app_selector/app_selector_view.dart';
+import 'package:revanced_manager/ui/views/app_selector/app_selector_viewmodel.dart';
 import 'package:revanced_manager/ui/views/home/home_view.dart';
 import 'package:revanced_manager/ui/views/patcher/patcher_view.dart';
+import 'package:revanced_manager/ui/views/patcher/patcher_viewmodel.dart';
 import 'package:revanced_manager/ui/views/patches_selector/patches_selector_view.dart';
+import 'package:revanced_manager/ui/views/patches_selector/patches_selector_viewmodel.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -14,5 +17,8 @@ import 'package:stacked_services/stacked_services.dart';
 ], dependencies: [
   LazySingleton(classType: NavigationService),
   LazySingleton(classType: PatcherAPI),
+  LazySingleton(classType: PatcherViewModel),
+  LazySingleton(classType: AppSelectorViewModel),
+  LazySingleton(classType: PatchesSelectorViewModel),
 ])
 class AppSetup {}
