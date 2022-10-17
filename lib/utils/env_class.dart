2aaed14a3aa4a5488bee3f3bbcbf272cc47fd258
Aunali321 <aunvakil.aa@gmail.com>
2022-10-17 13:59:39 +0530

    fix: add firebase options to repo.

--- lib/utils/env_class.dart
+++ lib/utils/env_class.dart
@@ -6,4 +6,19 @@ part 'env_class.g.dart';
 abstract class Env {
   @EnviedField(varName: 'sentryDSN')
   static const sentryDSN = _Env.sentryDSN;
+
+  @EnviedField(varName: 'apiKey')
+  static const apiKey = _Env.apiKey;
+
+  @EnviedField(varName: 'appId')
+  static const appId = _Env.appId;
+
+  @EnviedField(varName: 'messagingSenderI')
+  static const messagingSenderId = _Env.messagingSenderId;
+
+  @EnviedField(varName: 'projectId')
+  static const projectId = _Env.projectId;
+
+  @EnviedField(varName: 'storageBucket')
+  static const storageBucket = _Env.storageBucket;
 }
