b00d2d16d403f98d32b892b35e5ee8a71094bfd5
oSumAtrIX <johan.melkonyan1@web.de>
2022-11-01 22:02:51 +0100

    feat: simplify logging (#305)
    Co-authored-by: Ushie <ushiekane@gmail.com>

--- android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
+++ android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
@@ -180,7 +180,7 @@ class MainActivity : FlutterActivity() {
                 patcher.addPatches(patches)
                 patcher.executePatches().forEach { (patch, res) ->
                     if (res.isSuccess) {
-                        val msg = "[success] $patch"
+                        val msg = "Applied $patch"
                         handler.post {
                             installerChannel.invokeMethod(
                                 "update",
@@ -193,7 +193,7 @@ class MainActivity : FlutterActivity() {
                         }
                         return@forEach
                     }
-                    val msg = "[error] $patch:" + res.exceptionOrNull()!!.printStackTrace()
+                    val msg = "$patch failed.\nError:\n" + res.exceptionOrNull()!!.printStackTrace()
                     handler.post {
                         installerChannel.invokeMethod(
                             "update",
