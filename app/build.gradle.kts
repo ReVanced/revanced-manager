b372f7ee843cfee9c8086e5978b9c87158edea59
Canny <canny1913@outlook.com>
2023-01-28 02:00:52 +0300

    feat: initialize project

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+    google()
+}
+android {
+    namespace = "app.revanced.manager.compose"
+    compileSdk = 33
+    buildToolsVersion = "33.0.0"
+
+    defaultConfig {
+        applicationId = "app.revanced.manager.compose"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "0.0.1"
+
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures.compose = true
+
+    composeOptions.kotlinCompilerExtensionVersion = "1.3.2"
+}
+
+dependencies {
+
+    // AndroidX Core
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+
+    // Compose
+    val composeVersion = "1.3.3"
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    // Material 3
+    implementation("androidx.compose.material3:material3:1.0.1")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "ReVanced Manager"
+include(":app")
