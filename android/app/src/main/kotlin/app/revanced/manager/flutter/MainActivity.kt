a635e5b8d04d21cbc42cb33fa69374d68c46d3b8
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-03-02 14:51:35 +0700

    chore: `addFiles` -> `addIntegrations` (#725)
    fix #721

--- android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
+++ android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
@@ -150,7 +150,7 @@ class MainActivity : FlutterActivity() {
                             )
                         )
                     }
-                    patcher.addFiles(listOf(integrations)) {}
+                    patcher.addIntegrations(listOf(integrations)) {}
                 }
 
                 handler.post {
