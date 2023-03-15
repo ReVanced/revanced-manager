9e8c6383ccbbe5c3fec47be990542382bf56324e
Aunali321 <aunvakil.aa@gmail.com>
2023-03-15 20:33:08 +0530

    feat: restart app toast when changing sources, api url

--- lib/ui/views/settings/settingsFragment/settings_manage_sources.dart
+++ lib/ui/views/settings/settingsFragment/settings_manage_sources.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:revanced_manager/app/app.locator.dart';
 import 'package:revanced_manager/services/manager_api.dart';
+import 'package:revanced_manager/services/toast.dart';
 import 'package:revanced_manager/ui/widgets/settingsView/custom_text_field.dart';
 import 'package:revanced_manager/ui/widgets/settingsView/settings_tile_dialog.dart';
 import 'package:revanced_manager/ui/widgets/shared/custom_material_button.dart';
@@ -11,6 +12,7 @@ import 'package:stacked/stacked.dart';
 
 class SManageSources extends BaseViewModel {
   final ManagerAPI _managerAPI = locator<ManagerAPI>();
+  final Toast _toast = locator<Toast>();
 
   final TextEditingController _hostSourceController = TextEditingController();
   final TextEditingController _orgPatSourceController = TextEditingController();
@@ -124,6 +126,7 @@ class SManageSources extends BaseViewModel {
               _managerAPI.setIntegrationsRepo(
                 '${_orgIntSourceController.text}/${_intSourceController.text}',
               );
+              _toast.showBottom('settingsView.restartAppForChanges');
               Navigator.of(context).pop();
             },
           )
