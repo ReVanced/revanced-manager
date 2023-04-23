cc59d60dfde48bbd0ba61fbc321c961f3c2cfd85
Patryk Miś <patryk.mis@mykolab.com>
2023-04-23 14:36:52 +0200

    build: dependency and syntax updates (#17)
    * build: Update Gradle to v8.1.1
    
    * build: Bump dependencies
    
    * build: move repo configurations to settings
    
    ---------
    
    Co-authored-by: Patryk Mis <24607131+PatrickMis@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,29 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven("https://jitpack.io")
+        maven {
+            url = uri("https://maven.pkg.github.com/revanced/revanced-patcher")
+            credentials {
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
+        maven {
+            url = uri("https://maven.pkg.github.com/revanced/revanced-patcher")
+            credentials {
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
 rootProject.name = "ReVanced Manager"
