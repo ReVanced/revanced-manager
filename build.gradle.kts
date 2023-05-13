0871180dcc9f866d4797878a526ea04f444a08ef
Patryk Miś <foss@patrykmis.com>
2023-05-13 14:00:06 +0200

    build: updates (#21)
    * perf: obsolete sdk check
    
    Signed-off-by: Patryk Mis <foss@patrykmis.com>
    
    * chore: bump dependencies
    
    Signed-off-by: Patryk Mis <foss@patrykmis.com>
    
    ---------
    
    Signed-off-by: Patryk Mis <foss@patrykmis.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.android.application") version "8.0.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
 }
