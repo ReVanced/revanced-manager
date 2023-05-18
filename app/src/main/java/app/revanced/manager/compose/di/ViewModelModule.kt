4c3dbbd8d59fb59a54f5b5db2ca6f8e5d61564d1
CnC-Robert <CnC.Rob3rt@gmail.com>
2023-05-18 13:44:19 +0200

    feat: patches selector screen

--- app/src/main/java/app/revanced/manager/compose/di/ViewModelModule.kt
+++ app/src/main/java/app/revanced/manager/compose/di/ViewModelModule.kt
@@ -5,6 +5,6 @@ import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    //viewModelOf(::PatchesSelectorViewModel)
+    viewModelOf(::PatchesSelectorViewModel)
     viewModelOf(::SettingsViewModel)
 }
