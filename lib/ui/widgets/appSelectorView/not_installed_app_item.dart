d26b654eaf9750895b2ebc9b3b230bf59b17fc3a
Aabed Khan <aabedhkhan@gmail.com>
2023-06-10 12:15:45 +0545

    fix: fixed patch not showing if the app is not available on the Google Play Store.

--- lib/ui/widgets/appSelectorView/not_installed_app_item.dart
+++ lib/ui/widgets/appSelectorView/not_installed_app_item.dart
@@ -25,76 +25,78 @@ class _NotInstalledAppItem extends State<NotInstalledAppItem> {
     return FutureBuilder(
       future: getPackageInfo(widget.name),
       builder: (context, snapshot) {
-        if(snapshot.hasData) {
-          final Map<String, dynamic> data = snapshot.data!;
-          final String pkgName = widget.name;
-          final String iconUrl = data['image'];
-          final String name = data['name'];
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: CustomCard(
-              onTap: widget.onTap,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    width: 48,
-                    height: 48,
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    alignment: Alignment.center,
-                    child: ClipRRect(
-                        borderRadius:BorderRadius.circular(20),
-                        child: Image.network(iconUrl),
-                    ),
-                  ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          name,
-                          maxLines: 2,
-                          overflow: TextOverflow.visible,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: CustomCard(
+            onTap: widget.onTap,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 48,
+                  height: 48,
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  alignment: Alignment.center,
+                  child: (snapshot.hasData)
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(snapshot.data!['image']),
+                        )
+                      : const CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          child: Icon(
+                            Icons.square_rounded,
+                            color: Colors.grey,
+                            size: 44,
                           ),
                         ),
-                        const SizedBox(height: 4),
-                        const Text('App not installed'),
-                        Text(pkgName),
-                        Row(
-                          children: [
-                            Text(
-                              widget.suggestedVersion.isEmpty
-                                  ? 'All versions'
-                                  : widget.suggestedVersion,
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      if (snapshot.hasData)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 4.0),
+                          child: Text(
+                            snapshot.data!['name'],
+                            maxLines: 2,
+                            overflow: TextOverflow.visible,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
                             ),
-                            const SizedBox(width: 4),
-                            Text(
-                              widget.patchesCount == 1
-                                  ? '• ${widget.patchesCount} patch'
-                                  : '• ${widget.patchesCount} patches',
-                              style: TextStyle(
-                                color: Theme
-                                    .of(context)
-                                    .colorScheme
-                                    .secondary,
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
-                      ],
-                    ),
+                      Text(widget.name),
+                      const Text('App not installed'),
+                      Row(
+                        children: [
+                          Text(
+                            widget.suggestedVersion.isEmpty
+                                ? 'All versions'
+                                : widget.suggestedVersion,
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            widget.patchesCount == 1
+                                ? '• ${widget.patchesCount} patch'
+                                : '• ${widget.patchesCount} patches',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        }else{
-          return const SizedBox();
-        }},
+          ),
+        );
+      },
     );
   }
 }
