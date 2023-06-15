6309e8bdf5e9c25246544b1f2a8325dc71f5f30a
CnC-Robert <CnC.Rob3rt@gmail.com>
2023-06-15 22:20:17 +0200

    feat: filter options for patches

--- app/src/main/java/app/revanced/manager/ui/viewmodel/PatchesSelectorViewModel.kt
+++ app/src/main/java/app/revanced/manager/ui/viewmodel/PatchesSelectorViewModel.kt
@@ -15,19 +15,23 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 
 @Stable
-class PatchesSelectorViewModel(appInfo: AppInfo) : ViewModel(), KoinComponent {
+class PatchesSelectorViewModel(
+    val appInfo: AppInfo
+) : ViewModel(), KoinComponent {
+
     val bundlesFlow = get<SourceRepository>().bundles.map { bundles ->
         bundles.mapValues { (_, bundle) -> bundle.patches }.map { (name, patches) ->
             val supported = mutableListOf<PatchInfo>()
             val unsupported = mutableListOf<PatchInfo>()
+            val universal = mutableListOf<PatchInfo>()
 
             patches.filter { it.compatibleWith(appInfo.packageName) }.forEach {
-                val targetList = if (it.supportsVersion(appInfo.packageInfo!!.versionName)) supported else unsupported
+                val targetList = if (it.compatiblePackages == null) universal else if (it.supportsVersion(appInfo.packageInfo!!.versionName)) supported else unsupported
 
                 targetList.add(it)
             }
 
-            Bundle(name, supported, unsupported)
+            Bundle(name, supported, unsupported, universal)
         }
     }
 
@@ -45,24 +49,48 @@ class PatchesSelectorViewModel(appInfo: AppInfo) : ViewModel(), KoinComponent {
     }
 
     data class Bundle(
-        val name: String, val supported: List<PatchInfo>, val unsupported: List<PatchInfo>
+        val name: String,
+        val supported: List<PatchInfo>,
+        val unsupported: List<PatchInfo>,
+        val universal: List<PatchInfo>
     )
 
     var showOptionsDialog by mutableStateOf(false)
         private set
-    var showUnsupportedDialog by mutableStateOf(false)
-        private set
+
+    val compatibleVersions = mutableStateListOf<String>()
 
     fun dismissDialogs() {
         showOptionsDialog = false
-        showUnsupportedDialog = false
+        compatibleVersions.clear()
     }
 
     fun openOptionsDialog() {
         showOptionsDialog = true
     }
 
-    fun openUnsupportedDialog() {
-        showUnsupportedDialog = true
+    fun openUnsupportedDialog(unsupportedVersions: List<PatchInfo>) {
+        val set = HashSet<String>()
+
+        unsupportedVersions.forEach { patch ->
+            patch.compatiblePackages?.find { it.name == appInfo.packageName }?.let { compatiblePackage ->
+                set.addAll(compatiblePackage.versions)
+            }
+        }
+
+        compatibleVersions.addAll(set)
+    }
+
+    var filter by mutableStateOf(SHOW_SUPPORTED or SHOW_UNSUPPORTED)
+        private set
+
+    fun toggleFlag(flag: Int) {
+        filter = filter xor flag
+    }
+
+    companion object {
+        const val SHOW_SUPPORTED = 1 // 2^0
+        const val SHOW_UNIVERSAL = 2 // 2^1
+        const val SHOW_UNSUPPORTED = 4 // 2^2
     }
 }
