9a55e51a3ae31a374a199251fdeba97bdf67d92a
Ax333l <main@axelen.xyz>
2023-06-17 13:47:55 +0200

    build: bump patcher

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     ksp("androidx.room:room-compiler:$roomVersion")
 
     // ReVanced
-    implementation("app.revanced:revanced-patcher:9.0.0")
+    implementation("app.revanced:revanced-patcher:11.0.1")
 
     // Signing
     implementation("com.android.tools.build:apksig:8.2.0-alpha05")
