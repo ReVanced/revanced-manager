37e177b56e36aab46dbf5c214c2bf94a3c25e654
Pun Butrach <pun.butrach@gmail.com>
2023-07-07 23:14:27 +0700

    revert: downgrade Kotlin to 1.8.21
    "A what? 1.8.22 isn't compatible, but the version bump indicate that it's supposed to be bug fixes????"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "8.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    id("com.google.devtools.ksp") version "1.8.22-1.0.11" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
 }
