55573eb94fa1b414d9110b7e6278bf8dff3eb30c
Rom Reviewer <5242439+romreviewer@users.noreply.github.com>
2023-07-14 16:03:42 +0530

    chore: migrate dependencies to version catalogs (#58)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "8.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-    id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.devtools) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.about.libraries) apply false
 }
