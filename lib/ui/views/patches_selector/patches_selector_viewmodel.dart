a66fb90166a11f820ea89c88666c3ba26c9b3d55
aAbed <aabedhkhan@gmail.com>
2023-08-06 00:37:56 +0545

    refactor: removed unnecessary sorting

--- lib/ui/views/patches_selector/patches_selector_viewmodel.dart
+++ lib/ui/views/patches_selector/patches_selector_viewmodel.dart
@@ -27,13 +27,7 @@ class PatchesSelectorViewModel extends BaseViewModel {
         locator<PatcherViewModel>().selectedApp!.originalPackageName,
       ),
     );
-    patches.sort((a, b) {
-      if (a.compatiblePackages.isEmpty == b.compatiblePackages.isEmpty) {
-        return a.name.compareTo(b.name);
-      } else {
-        return a.compatiblePackages.isEmpty ? 1 : -1;
-      }
-    });
+    patches.sort((a, b) => a.name.compareTo(b.name));
     notifyListeners();
   }
 
