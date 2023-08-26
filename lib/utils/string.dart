70e8253b638fde061357754c91d56033cec0f64d
oSumAtrIX <johan.melkonyan1@web.de>
2023-08-26 17:59:52 +0200

    Migrate to compose branch
    This commit was made to allow cherry-picking the first commit of the followup commits according to https://github.com/ReVanced/revanced-manager-compose/issues/65#issue-1806335545

--- lib/utils/string.dart
+++
@@ -1,8 +0,0 @@
-extension StringCasingExtension on String {
-  String toCapitalized() =>
-      length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
-  String toTitleCase() => replaceAll(RegExp(' +'), ' ')
-      .split(' ')
-      .map((str) => str.toCapitalized())
-      .join(' ');
-}
