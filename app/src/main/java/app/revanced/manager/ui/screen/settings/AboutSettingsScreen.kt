9250df831af3fdd9311933be47125f0dbec567f2
Benjamin Halko <benjaminhalko@hotmail.com>
2023-09-21 16:02:05 -0700

    removed unused shape import

--- app/src/main/java/app/revanced/manager/ui/screen/settings/AboutSettingsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/settings/AboutSettingsScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FavoriteBorder
