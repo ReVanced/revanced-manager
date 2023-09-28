f60275859d6c5d8c3ef7019e96131d8b7238e4c2
Benjamin Halko <benjaminhalko@hotmail.com>
2023-09-28 09:40:39 -0700

    reset ViewModelModule

--- app/src/main/java/app/revanced/manager/di/ViewModelModule.kt
+++ app/src/main/java/app/revanced/manager/di/ViewModelModule.kt
@@ -1,21 +1,6 @@
 package app.revanced.manager.di
 
-import app.revanced.manager.ui.viewmodel.AboutViewModel
-import app.revanced.manager.ui.viewmodel.AdvancedSettingsViewModel
-import app.revanced.manager.ui.viewmodel.AppInfoViewModel
-import app.revanced.manager.ui.viewmodel.AppSelectorViewModel
-import app.revanced.manager.ui.viewmodel.ContributorViewModel
-import app.revanced.manager.ui.viewmodel.DashboardViewModel
-import app.revanced.manager.ui.viewmodel.DownloadsViewModel
-import app.revanced.manager.ui.viewmodel.ImportExportViewModel
-import app.revanced.manager.ui.viewmodel.InstalledAppsViewModel
-import app.revanced.manager.ui.viewmodel.InstallerViewModel
-import app.revanced.manager.ui.viewmodel.MainViewModel
-import app.revanced.manager.ui.viewmodel.ManagerUpdateChangelogViewModel
-import app.revanced.manager.ui.viewmodel.PatchesSelectorViewModel
-import app.revanced.manager.ui.viewmodel.SettingsViewModel
-import app.revanced.manager.ui.viewmodel.UpdateProgressViewModel
-import app.revanced.manager.ui.viewmodel.VersionSelectorViewModel
+import app.revanced.manager.ui.viewmodel.*
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
