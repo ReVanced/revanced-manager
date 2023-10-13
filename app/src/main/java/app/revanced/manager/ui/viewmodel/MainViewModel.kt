56a4a7043d8fb2ed1fb20baee2a42f5794724465
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2023-10-13 10:39:10 -0700

    feat: settings migration (compose) (#1309)

--- app/src/main/java/app/revanced/manager/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/app/revanced/manager/ui/viewmodel/MainViewModel.kt
@@ -1,21 +1,28 @@
 package app.revanced.manager.ui.viewmodel
 
+import android.util.Base64
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.revanced.manager.domain.bundles.PatchBundleSource.Companion.asRemoteOrNull
+import app.revanced.manager.domain.manager.KeystoreManager
 import app.revanced.manager.domain.manager.PreferencesManager
 import app.revanced.manager.domain.repository.PatchBundleRepository
-import kotlinx.coroutines.Dispatchers
+import app.revanced.manager.domain.repository.PatchSelectionRepository
+import app.revanced.manager.domain.repository.SerializedSelection
+import app.revanced.manager.ui.theme.Theme
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 class MainViewModel(
     private val patchBundleRepository: PatchBundleRepository,
+    private val patchSelectionRepository: PatchSelectionRepository,
+    private val keystoreManager: KeystoreManager,
     val prefs: PreferencesManager
 ) : ViewModel() {
-
     fun applyAutoUpdatePrefs(manager: Boolean, patches: Boolean) = viewModelScope.launch {
-        prefs.showAutoUpdatesDialog.update(false)
+        prefs.firstLaunch.update(false)
 
         prefs.managerAutoUpdates.update(manager)
         if (patches) {
@@ -30,4 +37,66 @@ class MainViewModel(
             }
         }
     }
+
+    fun applyLegacySettings(data: String) = viewModelScope.launch {
+        val json = Json { ignoreUnknownKeys = true }
+        val settings = json.decodeFromString<LegacySettings>(data)
+
+        settings.themeMode?.let { theme ->
+            val themeMap = mapOf(
+                0 to Theme.SYSTEM,
+                1 to Theme.LIGHT,
+                2 to Theme.DARK
+            )
+            prefs.theme.update(themeMap[theme]!!)
+        }
+        settings.useDynamicTheme?.let { dynamicColor ->
+            prefs.dynamicColor.update(dynamicColor)
+        }
+        settings.apiUrl?.let { api ->
+            prefs.api.update(api.removeSuffix("/"))
+        }
+        settings.experimentalPatchesEnabled?.let { allowExperimental ->
+            prefs.allowExperimental.update(allowExperimental)
+        }
+        settings.patchesAutoUpdate?.let { autoUpdate ->
+            with(patchBundleRepository) {
+                sources
+                    .first()
+                    .find { it.uid == 0 }
+                    ?.asRemoteOrNull
+                    ?.setAutoUpdate(autoUpdate)
+
+                updateCheck()
+            }
+        }
+        settings.patchesChangeEnabled?.let { disableSelectionWarning ->
+            prefs.disableSelectionWarning.update(disableSelectionWarning)
+        }
+        settings.keystore?.let { keystore ->
+            val keystoreBytes = Base64.decode(keystore, Base64.DEFAULT)
+            keystoreManager.import(
+                "ReVanced",
+                settings.keystorePassword,
+                keystoreBytes.inputStream()
+            )
+        }
+        settings.patches?.let { selection ->
+            patchSelectionRepository.import(0, selection)
+        }
+        prefs.firstLaunch.update(false)
+    }
+
+    @Serializable
+    private data class LegacySettings(
+        val keystorePassword: String,
+        val themeMode: Int? = null,
+        val useDynamicTheme: Boolean? = null,
+        val apiUrl: String? = null,
+        val experimentalPatchesEnabled: Boolean? = null,
+        val patchesAutoUpdate: Boolean? = null,
+        val patchesChangeEnabled: Boolean? = null,
+        val keystore: String? = null,
+        val patches: SerializedSelection? = null,
+    )
 }
