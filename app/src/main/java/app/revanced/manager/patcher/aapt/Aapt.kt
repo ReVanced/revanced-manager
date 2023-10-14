8f5449527d0a5a3028d78b1b27e9413efbdbca0d
Ax333l <main@axelen.xyz>
2023-10-14 18:24:40 +0200

    feat: armv7 warning

--- app/src/main/java/app/revanced/manager/patcher/aapt/Aapt.kt
+++ app/src/main/java/app/revanced/manager/patcher/aapt/Aapt.kt
@@ -1,9 +1,14 @@
 package app.revanced.manager.patcher.aapt
 
 import android.content.Context
+import android.os.Build.SUPPORTED_ABIS as DEVICE_ABIS
 import java.io.File
 
 object Aapt {
+    private val WORKING_ABIS = setOf("arm64-v8a", "x86", "x86_64")
+
+    fun supportsDevice() = (DEVICE_ABIS intersect WORKING_ABIS).isNotEmpty()
+
     fun binary(context: Context): File? {
         return File(context.applicationInfo.nativeLibraryDir).resolveAapt()
     }
