edb387e1a8234075318b39a15b118c5b833ae1f4
Ax333l <main@axelen.xyz>
2023-10-14 18:24:40 +0200

    feat: armv7 warning

--- app/src/main/java/app/revanced/manager/ui/screen/InstalledAppsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/InstalledAppsScreen.kt
@@ -2,10 +2,15 @@ package app.revanced.manager.ui.screen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.WarningAmber
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,14 +18,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import app.revanced.manager.R
 import app.revanced.manager.data.room.apps.installed.InstalledApp
+import app.revanced.manager.patcher.aapt.Aapt
 import app.revanced.manager.ui.component.AppIcon
 import app.revanced.manager.ui.component.AppLabel
 import app.revanced.manager.ui.component.LoadingIndicator
+import app.revanced.manager.ui.component.NotificationCard
 import app.revanced.manager.ui.viewmodel.InstalledAppsViewModel
 import org.koin.androidx.compose.getViewModel
 
@@ -31,43 +39,56 @@ fun InstalledAppsScreen(
 ) {
     val installedApps by viewModel.apps.collectAsStateWithLifecycle(initialValue = null)
 
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = installedApps?.let { if (it.isEmpty()) Arrangement.Center else Arrangement.Top } ?: Arrangement.Center
-    ) {
-        installedApps?.let { installedApps ->
-
-            if (installedApps.isNotEmpty()) {
-                items(
-                    installedApps,
-                    key = { it.currentPackageName }
-                ) { installedApp ->
-                    viewModel.packageInfoMap[installedApp.currentPackageName].let { packageInfo ->
-                         ListItem(
-                             modifier = Modifier.clickable { onAppClick(installedApp) },
-                             leadingContent = {
-                                 AppIcon(
-                                     packageInfo,
-                                     contentDescription = null,
-                                     Modifier.size(36.dp)
-                                 )
-                             },
-                             headlineContent = { AppLabel(packageInfo, defaultText = null) },
-                             supportingContent = { Text(installedApp.currentPackageName) }
-                         )
-
-                    }
-                }
-            } else {
-                item {
-                    Text(
-                        text = stringResource(R.string.no_patched_apps_found),
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                }
+    Column {
+        if (!Aapt.supportsDevice())
+            Box(modifier = Modifier.padding(16.dp)) {
+                NotificationCard(
+                    color = MaterialTheme.colorScheme.errorContainer,
+                    icon = Icons.Outlined.WarningAmber,
+                    text = stringResource(
+                        R.string.unsupported_architecture_warning
+                    ),
+                )
             }
 
-        } ?: item { LoadingIndicator() }
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = if (installedApps.isNullOrEmpty()) Arrangement.Center else Arrangement.Top
+        ) {
+            installedApps?.let { installedApps ->
+
+                if (installedApps.isNotEmpty()) {
+                    items(
+                        installedApps,
+                        key = { it.currentPackageName }
+                    ) { installedApp ->
+                        viewModel.packageInfoMap[installedApp.currentPackageName].let { packageInfo ->
+                            ListItem(
+                                modifier = Modifier.clickable { onAppClick(installedApp) },
+                                leadingContent = {
+                                    AppIcon(
+                                        packageInfo,
+                                        contentDescription = null,
+                                        Modifier.size(36.dp)
+                                    )
+                                },
+                                headlineContent = { AppLabel(packageInfo, defaultText = null) },
+                                supportingContent = { Text(installedApp.currentPackageName) }
+                            )
+
+                        }
+                    }
+                } else {
+                    item {
+                        Text(
+                            text = stringResource(R.string.no_patched_apps_found),
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
+                }
+
+            } ?: item { LoadingIndicator() }
+        }
     }
 }
