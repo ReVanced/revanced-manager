da404175c99507424d63050a68797b89611fbc8a
Ushie <ushiekane@gmail.com>
2023-10-15 20:47:43 +0300

    fix: restore correct custom card padding

--- lib/ui/widgets/shared/custom_card.dart
+++ lib/ui/widgets/shared/custom_card.dart
@@ -28,8 +28,7 @@ class CustomCard extends StatelessWidget {
         onTap: onTap,
         borderRadius: BorderRadius.circular(16),
         child: Padding(
-          padding:
-              padding ?? const EdgeInsets.only(top: 20.0, bottom: 20, left: 10),
+          padding: padding ?? const EdgeInsets.all(20.0),
           child: child,
         ),
       ),
