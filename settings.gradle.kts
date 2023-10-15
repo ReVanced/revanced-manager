db04672d72b42824f1591cf87818b111dc7a91b5
Ax333l <main@axelen.xyz>
2023-10-15 14:03:53 +0200

    chore: bump patcher

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,59 +1,19 @@
 pluginManagement {
     repositories {
-        // TODO: remove this once https://github.com/gradle/gradle/issues/23572 is fixed
-        val (gprUser, gprKey) = if (File(".gradle/gradle.properties").exists()) {
-            File(".gradle/gradle.properties").inputStream().use {
-                java.util.Properties().apply { load(it) }.let {
-                    it.getProperty("gpr.user") to it.getProperty("gpr.key")
-                }
-            }
-        } else {
-            null to null
-        }
-
-        fun RepositoryHandler.githubPackages(name: String) = maven {
-            url = uri(name)
-            credentials {
-                username = gprUser ?: providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = gprKey ?: providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-
         gradlePluginPortal()
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        githubPackages("https://maven.pkg.github.com/revanced/revanced-patcher")
-        githubPackages("https://maven.pkg.github.com/revanced/revanced-library")
+        mavenLocal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // TODO: remove this once https://github.com/gradle/gradle/issues/23572 is fixed
-        val (gprUser, gprKey) = if (File(".gradle/gradle.properties").exists()) {
-            File(".gradle/gradle.properties").inputStream().use {
-                java.util.Properties().apply { load(it) }.let {
-                    it.getProperty("gpr.user") to it.getProperty("gpr.key")
-                }
-            }
-        } else {
-            null to null
-        }
-
-        fun RepositoryHandler.githubPackages(name: String) = maven {
-            url = uri(name)
-            credentials {
-                username = gprUser ?: providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = gprKey ?: providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        githubPackages("https://maven.pkg.github.com/revanced/revanced-patcher")
-        githubPackages("https://maven.pkg.github.com/revanced/revanced-library")
+        mavenLocal()
     }
 }
 rootProject.name = "ReVanced Manager"
