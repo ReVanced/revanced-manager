57b1be5480ad13831fecf36b7893b63b5a08d287
Ushie <ushiekane@gmail.com>
2023-10-25 01:25:34 +0300

    feat: replace webview with a Markdown renderer

--- app/src/main/java/app/revanced/manager/ui/viewmodel/ManagerUpdateChangelogViewModel.kt
+++ app/src/main/java/app/revanced/manager/ui/viewmodel/ManagerUpdateChangelogViewModel.kt
@@ -12,28 +12,19 @@ import app.revanced.manager.network.api.ReVancedAPI
 import app.revanced.manager.network.utils.getOrThrow
 import app.revanced.manager.util.uiSafe
 import kotlinx.coroutines.launch
-import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
-import org.intellij.markdown.html.HtmlGenerator
-import org.intellij.markdown.parser.MarkdownParser
 
 class ManagerUpdateChangelogViewModel(
     private val api: ReVancedAPI,
     private val app: Application,
 ) : ViewModel() {
-    private val markdownFlavour = GFMFlavourDescriptor()
-    private val markdownParser = MarkdownParser(flavour = markdownFlavour)
-
     var changelog by mutableStateOf(
         Changelog(
             "...",
             app.getString(R.string.changelog_loading),
         )
     )
-        private set
-    val changelogHtml by derivedStateOf {
-        val markdown = changelog.body
-        val parsedTree = markdownParser.buildMarkdownTreeFromString(markdown)
-        HtmlGenerator(markdown, parsedTree, markdownFlavour).generateHtml()
+    val markdown by derivedStateOf {
+        changelog.body
     }
 
     init {
