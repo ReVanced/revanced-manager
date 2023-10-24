4827f7bd5a56b0fb407827a96b4d2da653d881c6
Ushie <ushiekane@gmail.com>
2023-10-24 17:33:52 +0300

    refactor: notification card overloads

--- app/src/main/java/app/revanced/manager/ui/component/NotificationCard.kt
+++ app/src/main/java/app/revanced/manager/ui/component/NotificationCard.kt
@@ -11,11 +11,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,131 +31,139 @@ fun NotificationCard(
     title: String? = null,
     text: String,
     icon: ImageVector,
-    onDismiss: (() -> Unit)? = null,
-    tertiaryAction: (@Composable () -> Unit)? = null,
-    actions: (@Composable () -> Unit)? = null
+    actions: (@Composable () -> Unit)?
 ) {
-    Card(
-        colors = CardDefaults.cardColors(containerColor = (if (isWarning) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.secondaryContainer)),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .clip(RoundedCornerShape(24.dp))
-    ) {
+    NotificationCardInstance(isWarning = isWarning) {
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(if (actions != null) 20.dp else 16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            modifier = Modifier.padding(if (title != null) 20.dp else 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            if (actions == null) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+            if (title != null) {
+                Icon(
+                    modifier = Modifier.size(36.dp),
+                    imageVector = icon,
+                    contentDescription = null
+                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(6.dp)
                 ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
+                    )
+                }
+            } else {
+                Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
                     Icon(
                         modifier = Modifier.size(24.dp),
                         imageVector = icon,
-                        contentDescription = null,
-                        tint = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
-                    )
-                    if (title == null) {
-                        Text(
-                            modifier = Modifier.weight(1f),
-                            text = text,
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    } else {
-                        Column(
-                            modifier = Modifier.weight(1f),
-                            verticalArrangement = Arrangement.spacedBy(6.dp)
-                        ) {
-                            Text(
-                                text = title,
-                                style = MaterialTheme.typography.titleLarge,
-                                color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
-                            )
-                            Text(
-                                text = text,
-                                style = MaterialTheme.typography.bodyMedium,
-                                color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
-                            )
-                        }
-                    }
-                    Row {
-                        tertiaryAction?.invoke()
-                        if (onDismiss != null) {
-                            IconButton(onClick = onDismiss) {
-                                Icon(
-                                    imageVector = Icons.Outlined.Close,
-                                    contentDescription = stringResource(R.string.close),
-                                    tint = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
-                                )
-                            }
-                        }
-                    }
-                }
-            } else {
-                if (title == null) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(16.dp)
-                    ) {
-                        Icon(
-                            modifier = Modifier.size(24.dp),
-                            imageVector = icon,
-                            contentDescription = null
-                        )
-                        Text(
-                            text = text,
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    }
-                } else {
-                    Icon(
-                        modifier = Modifier.size(36.dp),
-                        imageVector = icon,
                         contentDescription = null
                     )
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(6.dp)
-                    ) {
-                        Text(
-                            text = title,
-                            style = MaterialTheme.typography.titleLarge,
-                            color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
-                        )
-                        Text(
-                            text = text,
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
-                        )
-                    }
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Row {
-                        tertiaryAction?.invoke()
-                    }
-                    Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                        if (onDismiss != null) {
-                            TextButton(onClick = onDismiss) {
-                                Text(
-                                    text = stringResource(id = R.string.dismiss),
-                                    color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
-                                )
-                            }
-                        }
-                        actions.invoke()
-                    }
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
+                    )
                 }
             }
+
+            actions?.invoke()
+        }
+    }
+}
+
+@Composable
+fun NotificationCard(
+    isWarning: Boolean = false,
+    title: String? = null,
+    text: String,
+    icon: ImageVector,
+    onDismiss: (() -> Unit)? = null,
+    primaryAction: (() -> Unit)? = null
+) {
+    NotificationCardInstance(isWarning = isWarning, onClick = primaryAction) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Icon(
+                modifier = Modifier.size(if (title != null) 36.dp else 24.dp),
+                imageVector = icon,
+                contentDescription = null
+            )
+            if (title != null) {
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(6.dp)
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
+                    )
+                }
+            } else {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = text,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
+                )
+            }
+            if (onDismiss != null) {
+                IconButton(onClick = onDismiss) {
+                    Icon(
+                        imageVector = Icons.Outlined.Close,
+                        contentDescription = stringResource(R.string.close),
+                        tint = if (isWarning) MaterialTheme.colorScheme.onError else MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun NotificationCardInstance(
+    isWarning: Boolean = false,
+    onClick: (() -> Unit)? = null,
+    content: (@Composable () -> Unit),
+) {
+    if (onClick != null) {
+        Card(
+            onClick = { onClick },
+            colors = CardDefaults.cardColors(containerColor = (if (isWarning) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.secondaryContainer)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(24.dp))
+        ) {
+            content.invoke()
+        }
+    } else {
+        Card(
+            colors = CardDefaults.cardColors(containerColor = (if (isWarning) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.secondaryContainer)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(24.dp))
+        ) {
+            content.invoke()
         }
     }
 }
