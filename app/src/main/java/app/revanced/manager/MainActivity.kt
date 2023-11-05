1dc41badd9636362c72f0c1f9738c72aa926b286
Robert <72943079+CnC-Robert@users.noreply.github.com>
2023-11-05 12:19:55 +0000

    feat: check for updates on startup (#1462)

--- app/src/main/java/app/revanced/manager/MainActivity.kt
+++ app/src/main/java/app/revanced/manager/MainActivity.kt
@@ -1,27 +1,25 @@
 package app.revanced.manager
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Update
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import app.revanced.manager.ui.component.AutoUpdatesDialog
 import app.revanced.manager.ui.destination.Destination
-import app.revanced.manager.ui.screen.InstalledAppInfoScreen
+import app.revanced.manager.ui.destination.SettingsDestination
 import app.revanced.manager.ui.screen.AppSelectorScreen
 import app.revanced.manager.ui.screen.DashboardScreen
+import app.revanced.manager.ui.screen.InstalledAppInfoScreen
 import app.revanced.manager.ui.screen.InstallerScreen
 import app.revanced.manager.ui.screen.SelectedAppInfoScreen
 import app.revanced.manager.ui.screen.SettingsScreen
@@ -30,17 +28,15 @@ import app.revanced.manager.ui.theme.ReVancedManagerTheme
 import app.revanced.manager.ui.theme.Theme
 import app.revanced.manager.ui.viewmodel.MainViewModel
 import app.revanced.manager.ui.viewmodel.SelectedAppInfoViewModel
-import app.revanced.manager.util.tag
-import app.revanced.manager.util.toast
 import dev.olshevski.navigation.reimagined.AnimatedNavHost
 import dev.olshevski.navigation.reimagined.NavBackHandler
 import dev.olshevski.navigation.reimagined.navigate
 import dev.olshevski.navigation.reimagined.pop
 import dev.olshevski.navigation.reimagined.popUpTo
 import dev.olshevski.navigation.reimagined.rememberNavController
+import org.koin.core.parameter.parametersOf
 import org.koin.androidx.compose.getViewModel as getComposeViewModel
 import org.koin.androidx.viewmodel.ext.android.getViewModel as getAndroidViewModel
-import org.koin.core.parameter.parametersOf
 
 class MainActivity : ComponentActivity() {
     @ExperimentalAnimationApi
@@ -50,6 +46,8 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
 
         val vm: MainViewModel = getAndroidViewModel()
+
+        vm.importLegacySettings(this)
 
         setContent {
             val theme by vm.prefs.theme.getAsState()
@@ -66,46 +64,30 @@ class MainActivity : ComponentActivity() {
 
                 val firstLaunch by vm.prefs.firstLaunch.getAsState()
 
-                if (firstLaunch) {
-                    var legacyActivityState by rememberSaveable { mutableStateOf(LegacyActivity.NOT_LAUNCHED) }
-                    if (legacyActivityState == LegacyActivity.NOT_LAUNCHED) {
-                        val launcher = rememberLauncherForActivityResult(
-                            contract = ActivityResultContracts.StartActivityForResult()
-                        ) { result: ActivityResult ->
-                            if (result.resultCode == RESULT_OK) {
-                                if (result.data != null) {
-                                    val jsonData = result.data!!.getStringExtra("data")!!
-                                    vm.applyLegacySettings(jsonData)
+                if (firstLaunch) AutoUpdatesDialog(vm::applyAutoUpdatePrefs)
+
+                vm.updatedManagerVersion?.let {
+                    AlertDialog(
+                        onDismissRequest = vm::dismissUpdateDialog,
+                        confirmButton = {
+                            TextButton(
+                                onClick = {
+                                    vm.dismissUpdateDialog()
+                                    navController.navigate(Destination.Settings(SettingsDestination.UpdateProgress))
                                 }
-                            } else {
-                                legacyActivityState = LegacyActivity.FAILED
-                                toast(getString(R.string.legacy_import_failed))
+                            ) {
+                                Text(stringResource(R.string.update))
                             }
-                        }
-
-                        val intent = Intent().apply {
-                            setClassName(
-                                "app.revanced.manager.flutter",
-                                "app.revanced.manager.flutter.ExportSettingsActivity"
-                            )
-                        }
-
-                        LaunchedEffect(Unit) {
-                            try {
-                                launcher.launch(intent)
-                            } catch (e: Exception) {
-                                if (e !is ActivityNotFoundException) {
-                                    toast(getString(R.string.legacy_import_failed))
-                                    Log.e(tag, "Failed to launch legacy import activity: $e")
-                                }
-                                legacyActivityState = LegacyActivity.FAILED
+                        },
+                        dismissButton = {
+                            TextButton(onClick = vm::dismissUpdateDialog) {
+                                Text(stringResource(R.string.dismiss_temporary))
                             }
-                        }
-
-                        legacyActivityState = LegacyActivity.LAUNCHED
-                    } else if (legacyActivityState == LegacyActivity.FAILED) {
-                        AutoUpdatesDialog(vm::applyAutoUpdatePrefs)
-                    }
+                        },
+                        icon = { Icon(Icons.Outlined.Update, null) },
+                        title = { Text(stringResource(R.string.update_available)) },
+                        text = { Text(stringResource(R.string.update_available_description, it)) }
+                    )
                 }
 
                 AnimatedNavHost(
@@ -113,7 +95,7 @@ class MainActivity : ComponentActivity() {
                 ) { destination ->
                     when (destination) {
                         is Destination.Dashboard -> DashboardScreen(
-                            onSettingsClick = { navController.navigate(Destination.Settings) },
+                            onSettingsClick = { navController.navigate(Destination.Settings()) },
                             onAppSelectorClick = { navController.navigate(Destination.AppSelector) },
                             onAppClick = { installedApp ->
                                 navController.navigate(
@@ -138,7 +120,8 @@ class MainActivity : ComponentActivity() {
                         )
 
                         is Destination.Settings -> SettingsScreen(
-                            onBackClick = { navController.pop() }
+                            onBackClick = { navController.pop() },
+                            startDestination = destination.startDestination
                         )
 
                         is Destination.AppSelector -> AppSelectorScreen(
@@ -198,11 +181,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    private enum class LegacyActivity {
-        NOT_LAUNCHED,
-        LAUNCHED,
-        FAILED
     }
 }
