50387beb0f28d3ef78edc34aea1bb2278004a534
Ushie <ushiekane@gmail.com>
2023-11-07 14:54:51 +0300

    refactor: tidy up screen

--- app/src/main/java/app/revanced/manager/ui/screen/settings/update/UpdateScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/settings/update/UpdateScreen.kt
@@ -1,8 +1,10 @@
 package app.revanced.manager.ui.screen.settings.update
 
+import androidx.annotation.StringRes
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,13 +26,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import app.revanced.manager.BuildConfig
 import app.revanced.manager.R
 import app.revanced.manager.ui.component.AppTopBar
 import app.revanced.manager.ui.component.settings.Changelog
 import app.revanced.manager.ui.viewmodel.UpdateViewModel
+import app.revanced.manager.ui.viewmodel.UpdateViewModel.Changelog
+import app.revanced.manager.ui.viewmodel.UpdateViewModel.State
 import app.revanced.manager.util.formatNumber
 import app.revanced.manager.util.relativeTime
 import com.gigamole.composefadingedges.content.FadingEdgesContentType
@@ -38,7 +41,6 @@ import com.gigamole.composefadingedges.content.scrollconfig.FadingEdgesScrollCon
 import com.gigamole.composefadingedges.fill.FadingEdgesFillType
 import com.gigamole.composefadingedges.verticalFadingEdges
 import org.koin.androidx.compose.getViewModel
-import app.revanced.manager.ui.viewmodel.UpdateViewModel.State
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -63,148 +65,132 @@ fun UpdateScreen(
                 .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(32.dp)
         ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                when (vm.state) {
-                    State.CAN_DOWNLOAD -> {
-                        Text(
-                            text = stringResource(
-                                R.string.update_available
-                            ), style = MaterialTheme.typography.headlineMedium
-                        )
-                        Column {
-                            Text(
-                                text = stringResource(
-                                    id = R.string.current_version,
-                                    BuildConfig.VERSION_NAME
-                                ),
-                                style = MaterialTheme.typography.bodyMedium,
-                                color = MaterialTheme.colorScheme.onSurfaceVariant
-                            )
-                            if (vm.changelog != null) {
-                                Text(
-                                    text = stringResource(
-                                        id = R.string.new_version,
-                                        vm.changelog!!.version.replace("v", "")
-                                    ),
-                                    style = MaterialTheme.typography.bodyMedium,
-                                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                                )
-                            }
-                        }
-                    }
-
-                    State.DOWNLOADING -> {
-                        Text(
-                            text = stringResource(
-                                R.string.downloading_manager_update
-                            ), style = MaterialTheme.typography.headlineMedium
-                        )
-                        LinearProgressIndicator(
-                            progress = vm.downloadProgress,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        )
-                        Text(
-                            text = "${vm.downloadedSize.div(1000000)} MB /  ${
-                                vm.totalSize.div(
-                                    1000000
-                                )
-                            } MB (${
-                                vm.downloadProgress.times(100).toInt()
-                            }%)",
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = MaterialTheme.colorScheme.outline,
-                            modifier = Modifier.align(Alignment.CenterHorizontally),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-
-                    State.CAN_INSTALL -> {
-                        Text(
-                            text = stringResource(R.string.ready_to_install_update),
-                            style = MaterialTheme.typography.headlineMedium
-                        )
-                    }
-
-                    State.INSTALLING -> {
-                        Text(
-                            text = stringResource(R.string.installing_manager_update),
-                            style = MaterialTheme.typography.headlineMedium
-                        )
-                    }
-
-                    State.FAILED -> {
-                        Text(
-                            text = stringResource(R.string.install_update_manager_failed),
-                            style = MaterialTheme.typography.headlineMedium
-                        )
-                    }
-
-                    State.SUCCESS -> {
-                        Text(
-                            text = stringResource(R.string.update_completed),
-                            style = MaterialTheme.typography.headlineMedium
-                        )
-                    }
-                }
-            }
+            Header(vm)
             vm.changelog?.let { changelog ->
                 Divider()
-                val scrollState = rememberScrollState()
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .verticalScroll(scrollState)
-                        .verticalFadingEdges(
-                            fillType = FadingEdgesFillType.FadeColor(
-                                color = MaterialTheme.colorScheme.background,
-                                fillStops = Triple(0F, 0.55F, 1F),
-                                secondStopAlpha = 1F
-                            ),
-                            contentType = FadingEdgesContentType.Dynamic.Scroll(
-                                state = scrollState,
-                                scrollConfig = FadingEdgesScrollConfig.Dynamic(
-                                    animationSpec = spring(),
-                                    isLerpByDifferenceForPartialContent = true,
-                                    scrollFactor = 1.25F
-                                )
-                            ),
-                            length = 350.dp
-                        )
-                ) {
-                    Changelog(
-                        markdown = changelog.body.replace("`", ""),
-                        version = changelog.version,
-                        downloadCount = changelog.downloadCount.formatNumber(),
-                        publishDate = changelog.publishDate.relativeTime(LocalContext.current)
+                Changelog(changelog)
+            } ?: Spacer(modifier = Modifier.weight(1f))
+            Buttons(vm, onBackClick)
+        }
+    }
+}
+
+@Composable
+private fun Header(vm: UpdateViewModel) {
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Title(
+            when (vm.state) {
+                State.CAN_DOWNLOAD -> R.string.update_available
+                State.DOWNLOADING -> R.string.downloading_manager_update
+                State.CAN_INSTALL -> R.string.ready_to_install_update
+                State.INSTALLING -> R.string.installing_manager_update
+                State.FAILED -> R.string.install_update_manager_failed
+                State.SUCCESS -> R.string.update_completed
+            }
+        )
+        if (vm.state == State.CAN_DOWNLOAD) {
+            Column {
+                Text(
+                    text = stringResource(
+                        id = R.string.current_version,
+                        BuildConfig.VERSION_NAME
+                    ),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+                vm.changelog?.let { changelog ->
+                    Text(
+                        text = stringResource(
+                            id = R.string.new_version,
+                            changelog.version.replace("v", "")
+                        ),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                 }
-            } ?: Spacer(modifier = Modifier.weight(1f))
-            Row(
+            }
+        } else if (vm.state == State.DOWNLOADING) {
+            LinearProgressIndicator(
+                progress = vm.downloadProgress,
                 modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text =
+                "${vm.downloadedSize.div(1000000)} MB /  ${vm.totalSize.div(1000000)} MB (${
+                    vm.downloadProgress.times(
+                        100
+                    ).toInt()
+                }%)",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.outline,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
+
+@Composable
+private fun Title(@StringRes title: Int) {
+    Text(
+        text = stringResource(title),
+        style = MaterialTheme.typography.headlineMedium
+    )
+}
+
+@Composable
+private fun ColumnScope.Changelog(changelog: Changelog) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .weight(1f)
+            .verticalScroll(scrollState)
+            .verticalFadingEdges(
+                fillType = FadingEdgesFillType.FadeColor(
+                    color = MaterialTheme.colorScheme.background,
+                    fillStops = Triple(0F, 0.55F, 1F),
+                    secondStopAlpha = 1F
+                ),
+                contentType = FadingEdgesContentType.Dynamic.Scroll(
+                    state = scrollState,
+                    scrollConfig = FadingEdgesScrollConfig.Dynamic(
+                        animationSpec = spring(),
+                        isLerpByDifferenceForPartialContent = true,
+                        scrollFactor = 1.25F
+                    )
+                ),
+                length = 350.dp
+            )
+    ) {
+        Changelog(
+            markdown = (changelog.body + changelog.body).replace("`", ""),
+            version = changelog.version,
+            downloadCount = changelog.downloadCount.formatNumber(),
+            publishDate = changelog.publishDate.relativeTime(LocalContext.current)
+        )
+    }
+}
+
+@Composable
+private fun Buttons(vm: UpdateViewModel, onBackClick: () -> Unit) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        if (vm.state == State.DOWNLOADING || vm.state == State.CAN_INSTALL) {
+            TextButton(
+                onClick = onBackClick,
             ) {
-                if (vm.state == State.DOWNLOADING || vm.state == State.CAN_INSTALL) {
-                    TextButton(
-                        onClick = onBackClick,
-                    ) {
-                        Text(text = stringResource(R.string.cancel))
-                    }
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                if (vm.state == State.CAN_DOWNLOAD) {
-                    Button(onClick = vm::downloadUpdate) {
-                        Text(text = stringResource(R.string.update))
-                    }
-                } else if (vm.state == State.CAN_INSTALL) {
-                    Button(
-                        onClick = vm::installUpdate,
-                        enabled = vm.state == State.CAN_INSTALL
-                    ) {
-                        Text(text = stringResource(R.string.install_app))
-                    }
-                }
+                Text(text = stringResource(R.string.cancel))
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        if (vm.state == State.CAN_DOWNLOAD) {
+            Button(onClick = vm::downloadUpdate) {
+                Text(text = stringResource(R.string.update))
+            }
+        } else if (vm.state == State.CAN_INSTALL) {
+            Button(
+                onClick = vm::installUpdate,
+                enabled = vm.state == State.CAN_INSTALL
+            ) {
+                Text(text = stringResource(R.string.install_app))
             }
         }
     }
