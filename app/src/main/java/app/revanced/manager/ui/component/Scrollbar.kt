57504e22a82ec483d234aac4fe2c1bd0ee22a029
Ushie <ushiekane@gmail.com>
2023-11-11 02:19:52 +0300

    feat: thickness, padding and colors

--- app/src/main/java/app/revanced/manager/ui/component/Scrollbar.kt
+++ app/src/main/java/app/revanced/manager/ui/component/Scrollbar.kt
@@ -1,16 +1,19 @@
 package app.revanced.manager.ui.component
 
-import android.graphics.drawable.GradientDrawable.Orientation
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.gigamole.composescrollbars.Scrollbars
 import com.gigamole.composescrollbars.ScrollbarsState
 import com.gigamole.composescrollbars.config.ScrollbarsConfig
-import com.gigamole.composescrollbars.config.ScrollbarsConfigDefaults
 import com.gigamole.composescrollbars.config.ScrollbarsOrientation
+import com.gigamole.composescrollbars.config.layercontenttype.ScrollbarsLayerContentType
+import com.gigamole.composescrollbars.config.layersType.ScrollbarsLayersType
+import com.gigamole.composescrollbars.config.layersType.thicknessType.ScrollbarsThicknessType
 import com.gigamole.composescrollbars.config.visibilitytype.ScrollbarsVisibilityType
 import com.gigamole.composescrollbars.scrolltype.ScrollbarsScrollType
 import com.gigamole.composescrollbars.scrolltype.knobtype.ScrollbarsDynamicKnobType
@@ -22,6 +25,11 @@ fun Scrollbar(scrollState: ScrollState, modifier: Modifier = Modifier) {
         state = ScrollbarsState(
             ScrollbarsConfig(
                 orientation = ScrollbarsOrientation.Vertical,
+                paddingValues = PaddingValues(0.dp),
+                layersType = ScrollbarsLayersType.Wrap(ScrollbarsThicknessType.Exact(4.dp)),
+                knobLayerContentType = ScrollbarsLayerContentType.Default.Colored.Idle(
+                    idleColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.35f)
+                ),
                 visibilityType = ScrollbarsVisibilityType.Dynamic.Fade(
                     isVisibleOnTouchDown = true,
                     isStaticWhenScrollPossible = false
@@ -42,6 +50,11 @@ fun Scrollbar(scrollState: LazyListState, modifier: Modifier = Modifier) {
         state = ScrollbarsState(
             ScrollbarsConfig(
                 orientation = ScrollbarsOrientation.Vertical,
+                paddingValues = PaddingValues(0.dp),
+                layersType = ScrollbarsLayersType.Wrap(ScrollbarsThicknessType.Exact(4.dp)),
+                knobLayerContentType = ScrollbarsLayerContentType.Default.Colored.Idle(
+                    idleColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.35f)
+                ),
                 visibilityType = ScrollbarsVisibilityType.Dynamic.Fade(
                     isVisibleOnTouchDown = true,
                     isStaticWhenScrollPossible = false
