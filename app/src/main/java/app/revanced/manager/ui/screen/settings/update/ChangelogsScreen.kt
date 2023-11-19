bd9778a3d16ff7e97c65660045711a8e00923c04
Ushie <ushiekane@gmail.com>
2023-11-19 23:28:28 +0300

    feat(Update Screen): changelogs & handle states (#1464)
    Co-authored-by: Ax333l <main@axelen.xyz>

--- app/src/main/java/app/revanced/manager/ui/screen/settings/update/ChangelogsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/settings/update/ChangelogsScreen.kt
@@ -1,37 +1,27 @@
 package app.revanced.manager.ui.screen.settings.update
 
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.CalendarToday
-import androidx.compose.material.icons.outlined.Campaign
-import androidx.compose.material.icons.outlined.FileDownload
-import androidx.compose.material.icons.outlined.Sell
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import app.revanced.manager.R
 import app.revanced.manager.ui.component.AppTopBar
 import app.revanced.manager.ui.component.LoadingIndicator
-import app.revanced.manager.ui.component.Markdown
+import app.revanced.manager.ui.component.settings.Changelog
 import app.revanced.manager.ui.viewmodel.ChangelogsViewModel
 import app.revanced.manager.util.formatNumber
 import app.revanced.manager.util.relativeTime
@@ -102,77 +92,5 @@ fun ChangelogItem(
                 color = MaterialTheme.colorScheme.outlineVariant
             )
         }
-    }
-}
-
-@Composable
-private fun Changelog(
-    markdown: String,
-    version: String,
-    downloadCount: String,
-    publishDate: String
-) {
-    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 0.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                imageVector = Icons.Outlined.Campaign,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.primary,
-                modifier = Modifier
-                    .size(32.dp)
-            )
-            Text(
-                version.removePrefix("v"),
-                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight(800)),
-                color = MaterialTheme.colorScheme.primary,
-            )
-        }
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Tag(
-                Icons.Outlined.Sell,
-                version
-            )
-            Tag(
-                Icons.Outlined.FileDownload,
-                downloadCount
-            )
-            Tag(
-                Icons.Outlined.CalendarToday,
-                publishDate
-            )
-        }
-    }
-    Markdown(
-        markdown,
-    )
-}
-
-@Composable
-private fun Tag(icon: ImageVector, text: String) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(6.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = null,
-            modifier = Modifier.size(16.dp),
-            tint = MaterialTheme.colorScheme.outline,
-        )
-        Text(
-            text,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.outline,
-        )
     }
 }
