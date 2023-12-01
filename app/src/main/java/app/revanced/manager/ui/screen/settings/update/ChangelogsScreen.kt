169dbcbc35bc27bc52a0169a7eaf5c459efe040c
Robert <CnC.Rob3rt@gmail.com>
2023-12-01 20:56:54 +0100

    bruh

--- app/src/main/java/app/revanced/manager/ui/screen/settings/update/ChangelogsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/settings/update/ChangelogsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
