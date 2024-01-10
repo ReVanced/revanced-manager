6ec821df572b9e07c401f7fa96b05b54e53a7855
Ushie <ushiekane@gmail.com>
2024-01-10 15:34:49 +0300

    feat: Collapse ExtendedFAB on scroll

--- app/src/main/java/app/revanced/manager/util/Util.kt
+++ app/src/main/java/app/revanced/manager/util/Util.kt
@@ -11,6 +11,17 @@ import android.os.Build
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.graphics.Color
 import androidx.core.net.toUri
 import androidx.lifecycle.Lifecycle
@@ -165,4 +176,40 @@ fun String.relativeTime(context: Context): String {
     } catch (e: DateTimeParseException) {
         return context.getString(R.string.invalid_date)
     }
+}
+
+@Composable
+fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
+}
+
+@Composable
+fun ScrollState.isScrollingUp(): Boolean {
+    var previousValue by remember(this) { mutableStateOf(value.toFloat()) }
+    var isScrollingUpward by remember { mutableStateOf(true) }  // Track upward scrolling state
+
+    LaunchedEffect(this) {
+        snapshotFlow { isScrollInProgress }
+            .collect { isScrolling ->
+                if (isScrolling) {
+                    isScrollingUpward = value.toFloat() < previousValue
+                    previousValue = value.toFloat()  // Update previous value
+                }
+            }
+    }
+
+    return isScrollingUpward
 }
