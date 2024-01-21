ed1be36e7631d2ee0493d4f1227a1d24d5457184
Benjamin Halko <benjaminhalko@hotmail.com>
2024-01-21 11:31:34 -0800

    upgraded to font awesome 6

--- app/src/main/java/app/revanced/manager/ui/viewmodel/AboutViewModel.kt
+++ app/src/main/java/app/revanced/manager/ui/viewmodel/AboutViewModel.kt
@@ -1,20 +1,13 @@
 package app.revanced.manager.ui.viewmodel
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Language
-import androidx.compose.material.icons.outlined.MailOutline
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import app.revanced.manager.domain.manager.KeystoreManager
-import app.revanced.manager.network.api.MissingAssetException
 import app.revanced.manager.network.api.ReVancedAPI
 import app.revanced.manager.network.dto.ReVancedDonationLink
-import app.revanced.manager.network.dto.ReVancedRelease
 import app.revanced.manager.network.dto.ReVancedSocial
 import app.revanced.manager.network.utils.getOrNull
 import compose.icons.FontAwesomeIcons
@@ -24,6 +17,7 @@ import compose.icons.fontawesomeicons.brands.Github
 import compose.icons.fontawesomeicons.brands.Reddit
 import compose.icons.fontawesomeicons.brands.Telegram
 import compose.icons.fontawesomeicons.brands.Twitter
+import compose.icons.fontawesomeicons.brands.XTwitter
 import compose.icons.fontawesomeicons.brands.Youtube
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -53,7 +47,7 @@ class AboutViewModel(private val reVancedAPI: ReVancedAPI) : ViewModel() {
             "Reddit" to FontAwesomeIcons.Brands.Reddit,
             "Telegram" to FontAwesomeIcons.Brands.Telegram,
             "Twitter" to FontAwesomeIcons.Brands.Twitter,
-            "X" to FontAwesomeIcons.Brands.Twitter,
+            "X" to FontAwesomeIcons.Brands.XTwitter,
             "YouTube" to FontAwesomeIcons.Brands.Youtube,
         )
 
