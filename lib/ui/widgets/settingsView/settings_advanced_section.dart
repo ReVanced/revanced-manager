c7d975e612eabf11d2d66047d3994a8a3e7dd82e
aAbed <39409020+TheAabedKhan@users.noreply.github.com>
2024-01-25 23:05:34 +0545

    feat: Show a dialog when an update is available (#1654)

--- lib/ui/widgets/settingsView/settings_advanced_section.dart
+++ lib/ui/widgets/settingsView/settings_advanced_section.dart
@@ -7,6 +7,7 @@ import 'package:revanced_manager/ui/widgets/settingsView/settings_auto_update_pa
 import 'package:revanced_manager/ui/widgets/settingsView/settings_enable_patches_selection.dart';
 import 'package:revanced_manager/ui/widgets/settingsView/settings_require_suggested_app_version.dart';
 import 'package:revanced_manager/ui/widgets/settingsView/settings_section.dart';
+import 'package:revanced_manager/ui/widgets/settingsView/settings_show_update_dialog.dart';
 import 'package:revanced_manager/ui/widgets/settingsView/settings_universal_patches.dart';
 import 'package:revanced_manager/ui/widgets/settingsView/settings_version_compatibility_check.dart';
 
@@ -19,6 +20,7 @@ class SAdvancedSection extends StatelessWidget {
       title: 'settingsView.advancedSectionTitle',
       children: const <Widget>[
         SAutoUpdatePatches(),
+        SShowUpdateDialog(),
         SEnablePatchesSelection(),
         SRequireSuggestedAppVersion(),
         SVersionCompatibilityCheck(),
