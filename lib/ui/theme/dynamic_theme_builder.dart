b6214175a0add55dd3b152a7e6e1bc4c314595ba
surya-technovert <surya.m@technovert.com>
2024-06-10 16:51:48 +0530

    Revert "Added OnLifeCycleChange for theme."
    This reverts commit 7f29131867949dce6501f89821b1b987da31a9cf.

--- lib/ui/theme/dynamic_theme_builder.dart
+++ lib/ui/theme/dynamic_theme_builder.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:dynamic_themes/dynamic_themes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:revanced_manager/app/app.router.dart';
@@ -25,24 +24,12 @@ class DynamicThemeBuilder extends StatefulWidget {
 
 class _DynamicThemeBuilderState extends State<DynamicThemeBuilder>
     with WidgetsBindingObserver {
-  late Brightness _currentBrightness;
+  Brightness brightness = PlatformDispatcher.instance.platformBrightness;
 
   @override
   void initState() {
     super.initState();
-    _currentBrightness = PlatformDispatcher.instance.platformBrightness;
     WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      final systemBrightness = PlatformDispatcher.instance.platformBrightness;
-      if (_currentBrightness != systemBrightness) {
-        _currentBrightness = systemBrightness;
-        setState(() {});
-      }
-    }
   }
 
   @override
@@ -79,10 +66,10 @@ class _DynamicThemeBuilderState extends State<DynamicThemeBuilder>
         return DynamicTheme(
           themeCollection: ThemeCollection(
             themes: {
-              0: _currentBrightness == Brightness.light
+              0: brightness == Brightness.light
                   ? lightCustomTheme
                   : darkCustomTheme,
-              1: _currentBrightness == Brightness.light
+              1: brightness == Brightness.light
                   ? lightDynamicTheme
                   : darkDynamicTheme,
               2: lightCustomTheme,
