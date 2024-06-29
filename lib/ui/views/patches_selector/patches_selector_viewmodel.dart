7ef8f0454b61429a1e4e9d306535f23b2fca5ba7
aAbed <39409020+TheAabedKhan@users.noreply.github.com>
2024-06-29 18:25:20 +0545

    fix: Select previously applied patches when loading patch selection (#1865)

--- lib/ui/views/patches_selector/patches_selector_viewmodel.dart
+++ lib/ui/views/patches_selector/patches_selector_viewmodel.dart
@@ -301,9 +301,16 @@ class PatchesSelectorViewModel extends BaseViewModel {
 
   Future<void> loadSelectedPatches(BuildContext context) async {
     if (_managerAPI.isPatchesChangeEnabled()) {
-      final List<String> selectedPatches = await _managerAPI.getSelectedPatches(
-        locator<PatcherViewModel>().selectedApp!.packageName,
-      );
+      final List<String>? appliedPatches = _managerAPI
+          .getPatchedApps()
+          .firstWhereOrNull(
+            (app) => app.packageName == selectedApp!.packageName,
+          )
+          ?.appliedPatches;
+      final List<String> selectedPatches = appliedPatches ??
+          await _managerAPI.getSelectedPatches(
+            selectedApp!.packageName,
+          );
       if (selectedPatches.isNotEmpty) {
         this.selectedPatches.clear();
         this.selectedPatches.addAll(
