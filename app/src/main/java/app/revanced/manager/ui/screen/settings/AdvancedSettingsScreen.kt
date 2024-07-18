64c8f8f3249b06a99711e6167c6038d130d50756
Ushie <ushiekane@gmail.com>
2024-07-18 04:21:18 +0300

    chore: Remove unneeded `@Preview`

--- app/src/main/java/app/revanced/manager/ui/screen/settings/AdvancedSettingsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/settings/AdvancedSettingsScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.getSystemService
 import androidx.lifecycle.viewModelScope
@@ -161,12 +160,6 @@ fun AdvancedSettingsScreen(
             )
         }
     }
-}
-
-@Preview
-@Composable
-private fun APIUrlDialogPreview() {
-    APIUrlDialog("https://example.com") {}
 }
 
 @Composable
