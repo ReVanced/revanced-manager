f30e2691877fdcbeb864cb6fbbb88eccee258b03
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-08-16 14:10:09 +0900

    feat: Open the app-specific Manage all files permission dialog

--- app/src/main/java/app/revanced/manager/util/RequestManageStorageContract.kt
+++ app/src/main/java/app/revanced/manager/util/RequestManageStorageContract.kt
@@ -2,6 +2,7 @@ package app.revanced.manager.util
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.Settings
@@ -10,7 +11,7 @@ import androidx.annotation.RequiresApi
 
 @RequiresApi(Build.VERSION_CODES.R)
 class RequestManageStorageContract(private val forceLaunch: Boolean = false) : ActivityResultContract<String, Boolean>() {
-    override fun createIntent(context: Context, input: String) = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+    override fun createIntent(context: Context, input: String) = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, Uri.fromParts("package", context.packageName, null))
 
     override fun getSynchronousResult(context: Context, input: String): SynchronousResult<Boolean>? = if (!forceLaunch && Environment.isExternalStorageManager()) SynchronousResult(true) else null
 
