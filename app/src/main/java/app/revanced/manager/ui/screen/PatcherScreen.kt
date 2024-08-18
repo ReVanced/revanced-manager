d54604506694d04a1512062086679365f5d36873
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-08-18 15:01:54 +0900

    fix: Screen turns off while patching due to wrong WakeLock

--- app/src/main/java/app/revanced/manager/ui/screen/PatcherScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/PatcherScreen.kt
@@ -1,5 +1,7 @@
 package app.revanced.manager.ui.screen
 
+import android.app.Activity
+import android.view.WindowManager
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts.CreateDocument
@@ -25,6 +27,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -82,6 +85,16 @@ fun PatcherScreen(
             val total = vm.steps.size - 1 + patchesTotal
 
             current.toFloat() / total.toFloat()
+        }
+    }
+
+    if (patcherSucceeded == null) {
+        DisposableEffect(Unit) {
+            val window = (context as Activity).window
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            onDispose {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            }
         }
     }
 
