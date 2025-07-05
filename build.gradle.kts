e85efceb3980a1fd3fd443208742e329e5f875a4
Pun Butrach <pun.butrach@gmail.com>
2025-07-06 02:13:33 +0700

    build: Use ktlint (Android Studio configuration)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,6 @@ spotless {
     ratchetFrom("compose-dev")
     kotlin {
         target("app/src/*/java/**/*.kt", "api/src/*/kotlin/**/*.kt")
-        ktfmt().googleStyle()
+        ktlint().setEditorConfigPath("$projectDir/.editorconfig")
     }
 }
