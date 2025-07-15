c26adf5dca309ab3510cb600fd47c42f295424f9
Pun Butrach <pun.butrach@gmail.com>
2025-07-15 23:10:24 +0700

    chore: Ratcheteer!
    This reverts commit b1e5f2534145edaeefac847ca84572b8e72a0bdf.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 }
 
 spotless {
+    ratchetFrom("compose-dev")
     kotlin {
         target("app/src/*/java/**/*.kt", "api/src/*/kotlin/**/*.kt")
         ktlint().setEditorConfigPath("$projectDir/.editorconfig")
