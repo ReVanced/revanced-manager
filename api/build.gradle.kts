5fdf98bef67135b213815a611b60b6efd6d5475e
Pun Butrach <pun.butrach@gmail.com>
2025-12-29 00:19:33 +0700

    chore: Remove missing proguard configuration

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -23,17 +23,11 @@ android {
 
     defaultConfig {
         minSdk = 26
-
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 
