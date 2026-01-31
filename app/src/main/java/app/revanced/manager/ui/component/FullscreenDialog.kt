b12fae20a88b32101748d7a2ba13793431427e98
planshim <100317079+planshim@users.noreply.github.com>
2026-01-31 08:26:07 +0300

    fix: make FullscreenDialog follow app theme override

--- app/src/main/java/app/revanced/manager/ui/component/FullscreenDialog.kt
+++ app/src/main/java/app/revanced/manager/ui/component/FullscreenDialog.kt
@@ -3,7 +3,8 @@ package app.revanced.manager.ui.component
 import android.view.WindowManager
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
@@ -11,6 +12,9 @@ import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.DialogWindowProvider
 import androidx.core.view.WindowCompat
+import app.revanced.manager.domain.manager.PreferencesManager
+import app.revanced.manager.ui.theme.Theme
+import org.koin.compose.koinInject
 
 private val properties = DialogProperties(
     usePlatformDefaultWidth = false,
@@ -20,13 +24,17 @@ private val properties = DialogProperties(
 
 @Composable
 fun FullscreenDialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit) {
+    val prefs: PreferencesManager = koinInject()
+    val theme by prefs.theme.getAsState()
+    val isDarkTheme = theme == Theme.SYSTEM && isSystemInDarkTheme() || theme == Theme.DARK
+    
     Dialog(
         onDismissRequest = onDismissRequest,
         properties = properties
     ) {
         val view = LocalView.current
-        val isDarkTheme = isSystemInDarkTheme()
-        LaunchedEffect(isDarkTheme) {
+        
+        SideEffect {
             val window = (view.parent as DialogWindowProvider).window
             window.statusBarColor = Color.Transparent.toArgb()
             window.navigationBarColor = Color.Transparent.toArgb()
