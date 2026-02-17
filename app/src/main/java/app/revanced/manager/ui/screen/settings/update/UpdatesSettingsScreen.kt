5cb0a9e7c5979619ebe4a5974a77ac99949ed332
planshim <100317079+planshim@users.noreply.github.com>
2026-02-18 01:31:33 +0300

    feat: new updates settings screen design

--- app/src/main/java/app/revanced/manager/ui/screen/settings/update/UpdatesSettingsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/settings/update/UpdatesSettingsScreen.kt
@@ -1,18 +1,38 @@
 package app.revanced.manager.ui.screen.settings.update
 
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Update
 import androidx.compose.material.icons.outlined.WorkOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalResources
@@ -20,16 +40,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import app.revanced.manager.R
 import app.revanced.manager.ui.component.AppTopBar
+import app.revanced.manager.ui.component.BottomContentBar
 import app.revanced.manager.ui.component.ColumnWithScrollbar
 import app.revanced.manager.ui.component.ListSection
 import app.revanced.manager.ui.component.settings.BooleanItem
-import app.revanced.manager.ui.component.settings.SettingsListItem
 import app.revanced.manager.ui.viewmodel.UpdatesSettingsViewModel
 import app.revanced.manager.util.toast
+import com.google.accompanist.drawablepainter.rememberDrawablePainter
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun UpdatesSettingsScreen(
     onBackClick: () -> Unit,
@@ -41,6 +62,11 @@ fun UpdatesSettingsScreen(
     val resources = LocalResources.current
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    val appIcon = rememberDrawablePainter(
+        drawable = remember(context) {
+            AppCompatResources.getDrawable(context, R.drawable.ic_logo_ring)
+        }
+    )
 
     Scaffold(
         topBar = {
@@ -49,6 +75,31 @@ fun UpdatesSettingsScreen(
                 scrollBehavior = scrollBehavior,
                 onBackClick = onBackClick
             )
+        },
+        bottomBar = {
+            BottomContentBar {
+                FilledTonalButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    onClick = {
+                        coroutineScope.launch {
+                            if (!vm.isConnected) {
+                                context.toast(resources.getString(R.string.no_network_toast))
+                                return@launch
+                            }
+                            if (vm.checkForUpdates()) onUpdateClick()
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Update,
+                        contentDescription = stringResource(R.string.refresh)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = stringResource(R.string.manual_update_check))
+                }
+            }
         },
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
     ) { paddingValues ->
@@ -61,32 +112,55 @@ fun UpdatesSettingsScreen(
                 title = stringResource(R.string.manager),
                 leadingContent = { Icon(Icons.Outlined.WorkOutline, contentDescription = null, modifier = Modifier.size(18.dp)) }
             ) {
-                SettingsListItem(
-                    headlineContent = stringResource(R.string.manual_update_check),
-                    supportingContent = stringResource(R.string.manual_update_check_description),
-                    onClick = {
-                        coroutineScope.launch {
-                            if (!vm.isConnected) {
-                                context.toast(resources.getString(R.string.no_network_toast))
-                                return@launch
-                            }
-                            if (vm.checkForUpdates()) onUpdateClick()
+                Surface(
+                    shape = RoundedCornerShape(4.dp),
+                    color = MaterialTheme.colorScheme.surfaceContainerLow,
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(vertical = 16.dp)
+                    ) {
+                        ListItem(
+                            headlineContent = {
+                                Text(stringResource(R.string.app_name))
+                            },
+                            leadingContent = {
+                                Image(
+                                    painter = appIcon,
+                                    contentDescription = stringResource(R.string.app_name),
+                                    modifier = Modifier.size(42.dp)
+                                )
+                            },
+                            colors = ListItemDefaults.colors(
+                                containerColor = Color.Transparent
+                            )
+                        )
+                        Button(
+                            onClick = {
+                                if (!vm.isConnected) {
+                                    context.toast(resources.getString(R.string.no_network_toast))
+                                } else {
+                                    onChangelogClick()
+                                }
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                                contentColor = MaterialTheme.colorScheme.onSurface
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp)
+                        ) {
+                            Text(text = stringResource(R.string.changelog))
                         }
                     }
-                )
+                }
+            }
 
-                SettingsListItem(
-                    headlineContent = stringResource(R.string.changelog),
-                    supportingContent = stringResource(R.string.changelog_description),
-                    onClick = {
-                        if (!vm.isConnected) {
-                            context.toast(resources.getString(R.string.no_network_toast))
-                            return@SettingsListItem
-                        }
-                        onChangelogClick()
-                    }
-                )
+            Spacer(modifier = Modifier.height(16.dp))
 
+            ListSection {
                 BooleanItem(
                     preference = vm.managerAutoUpdates,
                     headline = R.string.update_checking_manager,
