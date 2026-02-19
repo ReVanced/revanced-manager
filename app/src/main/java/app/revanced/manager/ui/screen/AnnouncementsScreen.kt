2e8b0fd28fc88f645e8e583d9f24ea5051fdff77
Xinto <burtkni@yandex.ru>
2026-02-20 00:33:28 +0400

    Cleanup imports

--- app/src/main/java/app/revanced/manager/ui/screen/AnnouncementsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/AnnouncementsScreen.kt
@@ -1,6 +1,5 @@
 package app.revanced.manager.ui.screen
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,13 +12,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.FilterAlt
 import androidx.compose.material3.Badge
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
