687514c71b6fed2db47ad2070772b42e5321ccaf
Xinto <burtkni@yandex.ru>
2026-02-19 22:43:39 +0400

    Remove references to external storage permissions

--- app/src/main/java/app/revanced/manager/data/platform/Filesystem.kt
+++ app/src/main/java/app/revanced/manager/data/platform/Filesystem.kt
@@ -1,18 +1,10 @@
 package app.revanced.manager.data.platform
 
-import android.Manifest
 import android.app.Application
 import android.content.Context
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
-import android.os.Environment
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.documentfile.provider.DocumentFile
-import app.revanced.manager.util.RequestManageStorageContract
 import java.io.File
-import java.nio.file.Path
 
 class Filesystem(private val app: Application) {
     val contentResolver = app.contentResolver // TODO: move Content Resolver operations to here.
@@ -40,22 +32,4 @@ class Filesystem(private val app: Application) {
     fun openFolderDocument(uri: Uri): DocumentFile? {
         return DocumentFile.fromTreeUri(app, uri)
     }
-
-    fun externalFilesDir(): Path = Environment.getExternalStorageDirectory().toPath()
-
-    private fun usesManagePermission() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
-
-    private val storagePermissionName =
-        if (usesManagePermission()) Manifest.permission.MANAGE_EXTERNAL_STORAGE else Manifest.permission.READ_EXTERNAL_STORAGE
-
-    fun permissionContract(): Pair<ActivityResultContract<String, Boolean>, String> {
-        val contract =
-            if (usesManagePermission()) RequestManageStorageContract() else ActivityResultContracts.RequestPermission()
-        return contract to storagePermissionName
-    }
-
-    fun hasStoragePermission() =
-        if (usesManagePermission()) Environment.isExternalStorageManager() else app.checkSelfPermission(
-            storagePermissionName
-        ) == PackageManager.PERMISSION_GRANTED
 }
