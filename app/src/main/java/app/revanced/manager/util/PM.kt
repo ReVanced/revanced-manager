c10342ca48a3828187596a10e49c52e2f44cd901
Kofhisho <github@alphexo.dev>
2026-03-28 01:45:24 +0000

    fix: nullable isNullOrEmpty revert
    Co-authored-by: Ushie <ushiekane@gmail.com>

--- app/src/main/java/app/revanced/manager/util/PM.kt
+++ app/src/main/java/app/revanced/manager/util/PM.kt
@@ -171,7 +171,7 @@ class PM(
  * Split APKs cannot be used directly as a patch source.
  */
 fun PackageInfo.isSplitApk(): Boolean =
-    !applicationInfo?.splitSourceDirs.isNullOrEmpty() || splitNames.isNotEmpty()
+    !applicationInfo?.splitSourceDirs.isNullOrEmpty() || !splitNames.isNullOrEmpty()
 
 /**
  * Returns true if this package is a system app.
