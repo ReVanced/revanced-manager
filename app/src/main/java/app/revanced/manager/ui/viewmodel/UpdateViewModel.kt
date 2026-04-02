48f11ecfb31cfe75bb4358266039a16c6444af73
Ushie <ushiekane@gmail.com>
2026-04-02 23:15:16 +0300

    deduplicate

--- app/src/main/java/app/revanced/manager/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/app/revanced/manager/ui/viewmodel/UpdateViewModel.kt
@@ -218,7 +218,7 @@ class UpdateViewModel(
     override fun onCleared() {
         super.onCleared()
         if (installerSessionId == null) {
-            location.delete()
+            cancelUpdate()
         }
     }
 
