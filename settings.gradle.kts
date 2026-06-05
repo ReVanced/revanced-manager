9e592df926b5ad0772b511b8b91747d482aff289
TUSHAR CHOUDHARY <tushar123913@gmail.com>
2026-06-05 18:09:38 +0530

    Add TV PR notes; bump Gradle; add Foojay plugin
    Add TV_PR_NOTES.md documenting the Android TV support PR (#3385) with maintainer feedback, QA steps for TV emulators, and ready-to-use reply text. Update gradle/wrapper/gradle-wrapper.properties to use Gradle 9.5 (update distributionUrl and remove obsolete wrapper entries). Add the Foojay toolchain resolver plugin declaration in settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
