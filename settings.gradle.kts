84b344a39367b0ea2945649528430caa7f7e6f15
TUSHAR CHOUDHARY <tushar123913@gmail.com>
2026-06-06 08:04:58 +0530

    revert unrelated changes: .gitignore, settings.gradle.kts, and gradle-wrapper.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
